dc3a85a7396759fe8ef38f0bb63af1ca70bf7c1f
Kosuke1477 <66466878+Kosuke1477@users.noreply.github.com>
2020-07-21 22:55:25 +0900

    Task17

--- app/Profile.php
+++ app/Profile.php
@@ -8,16 +8,11 @@ class Profile extends Model
 {
     protected $guarded = array('id');
 
+    // 以下を追記
     public static $rules = array(
         'name' => 'required',
         'gender' => 'required',
         'hobby' => 'required',
         'introduction' => 'required',
     );
-
-    public function phistories()
-    {
-      return $this->hasMany('App\Phistory');
-
-    }
 }
